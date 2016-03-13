d99b4623f824708df97d07f64d759f29274e9afe
kusabashira <kusabashira227@gmail.com>
2016-03-13 17:51:55 +0900

    Interpret backslash as escape-marker in toDelimiters

--- separator.go
+++ separator.go
@@ -1,8 +1,29 @@
 package main
 
 func toDelimiters(list string) []string {
+	var isEscaping bool
 	var a []string
 	for _, ch := range list {
+		if isEscaping {
+			switch ch {
+			case '\\':
+				a = append(a, "\\")
+			case 't':
+				a = append(a, "\t")
+			case 'n':
+				a = append(a, "\n")
+			case '0':
+				a = append(a, "")
+			default:
+				a = append(a, string(ch))
+			}
+			isEscaping = false
+			continue
+		}
+		if ch == '\\' {
+			isEscaping = true
+			continue
+		}
 		a = append(a, string(ch))
 	}
 	if len(a) == 0 {
